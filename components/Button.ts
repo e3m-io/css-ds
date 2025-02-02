981f47c085199b58cae8806876284d8d7dd23d36
e3m-io <66180948+e3m-io@users.noreply.github.com>
2025-02-01 21:26:29 -0800

    feat: add more bg and text classes

--- components/Button.ts
+++ components/Button.ts
@@ -1,30 +1,33 @@
 export type ButtonProps = {
 	variant: "normal" | "warning" | "danger";
 	weight: "primary" | "secondary" | "tertiary";
-}
+};
 
 const buttonClassMatrix = {
-	"normal": {
-		"primary": "bg-brand-solid",
-		"secondary": "bg-primary border-thin",
-		"tertiary": ""
+	normal: {
+		primary: "bg-brand-solid",
+		secondary: "bg-primary",
+		tertiary: "text-brand",
 	} as const,
-	"warning": {
-		"primary": "bg-warning-solid",
-		"secondary": "",
-		"tertiary": ""
+	warning: {
+		primary: "bg-warning-solid",
+		secondary: "bg-warning",
+		tertiary: "text-warning",
 	} as const,
-	"danger": {
-		"primary": "bg-danger-solid",
-		"secondary": "",
-		"tertiary": ""
-	} as const
-} satisfies Record<ButtonProps["variant"], Record<ButtonProps["weight"], string>>
+	danger: {
+		primary: "bg-danger-solid",
+		secondary: "bg-danger",
+		tertiary: "text-danger",
+	} as const,
+} satisfies Record<
+	ButtonProps["variant"],
+	Record<ButtonProps["weight"], string>
+>;
 
-export const getButtonClasses = (
-	props: ButtonProps
-) => `btn hover focus-visible ${buttonClassMatrix[props.variant][props.weight]}` as const;
+export const getButtonClasses = (props: ButtonProps) =>
+	`btn hover focus-visible text-weight--emphasized ${
+		buttonClassMatrix[props.variant][props.weight]
+	}` as const;
 
-export const getIconButtonClasses = (
-	props: ButtonProps
-) => `${getButtonClasses(props)} btn--icon-only` as const;
+export const getIconButtonClasses = (props: ButtonProps) =>
+	`${getButtonClasses(props)} btn--icon-only` as const;
